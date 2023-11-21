74591c715ef0d98270e854327254125fe0d7893b
Pranav Raju <pranavraju2020@gmail.com>
2023-11-21 18:55:12 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import Login from "./Login";
+
+
 
 const App = () => {
+
+  const [isLoggedIn,setIsLoggedIn] = useState(false); 
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Parent Component</h1>
+        <Login isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>
     </div>
   )
 }
